5ac7eb80bcf370db3f99ec185f9cb5b9bdb17acc
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-03-22 15:41:00 -0300

    Add files via upload

---
+++ plaindromo.21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string valor = "";
+        foreach (string s in System.IO.File.ReadLines(@"C:\Users\alberto.barbosa\Desktop\aquivo txt\txt.palindromo.txt"))
+        {
+            string word, revs = "";
+            word = s;
+            if (word != null)
+            {
+                for (int i = s.Length - 1; i >= 0; i--) //le a string de maneira reversa
+                {
+
+                    revs += word[i].ToString();
+                }
+                if (revs == word) // aqui checa se é palindromo ou não
+                {
+                    valor = "A palavra " + word + " é um palindromo";
+                    Console.WriteLine(valor); // se for a mesma palavra de trás para frente
+                }
+                else
+                {
+                    valor = "A palavra " + word + " não é um palindromo";
+                    Console.WriteLine(valor); // se a palavra não for a mesma
+                }
+                using (StreamWriter writer = new StreamWriter(@"C:\Users\alberto.barbosa\Desktop\aquivo txt\resposta.txt", true))
+                {
+                    writer.WriteLine(valor);
+                }
+            }
+        }
+    }
+}
+
